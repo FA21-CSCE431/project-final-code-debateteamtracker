bc3f9901cd118befadf9565abdb79b7df9d61001
Harini Maheswaran <harinimaheswaran@Harinis-MacBook-Pro.local>
2021-10-25 09:01:28 -0500

    add bounds for integer attributes

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,7 @@
 class Member < ApplicationRecord
     validates :email, presence: true
     validates :name, presence: true
-    validates :points, presence: true
-    validates :priority, presence: true
+    validates :points, :numericality => { :greater_than_or_equal_to => 0 }, presence: true
+    validates :priority, :numericality => { :greater_than_or_equal_to => 0 }, presence: true
+    validates :priority, :numericality => { :less_than_or_equal_to => 3 }, presence: true
 end
