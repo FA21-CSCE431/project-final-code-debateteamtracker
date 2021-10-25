9a3d7ff84949187bc9348d29025ff0d09e0f98e8
Harini Maheswaran <harinimaheswaran@Harinis-MacBook-Pro.local>
2021-10-25 09:00:32 -0500

    Add authentication for admin

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,11 @@
 class MembersController < ApplicationController
   before_action :set_member, only: %i[ show edit update destroy ]
+  before_action :authenticate_admin!
+  before_action :authenticate_permission
+
+  def authenticate_permission
+    redirect_to '/', alert: 'Not authorized. This email does not have admin permissions' unless Member.exists?(['email LIKE ? AND priority = 3', "%#{current_admin.email}"])
+  end
 
   # GET /members or /members.json
   def index
