0b0cd852d608ea7ffa7cb62c9b4bbd2019c99331
root <root@localhost>
2021-10-26 03:37:43 +0000

    recovering important filesgit add .git add .

---
+++ app/mailers/group_mail_mailer.rb
@@ -0,0 +1,12 @@
+class GroupMailMailer < ApplicationMailer
+    def email_layout
+        @announcement = params[:announcement]
+
+        mail(to: "dservin101@gmail.com", subject: @announcement.title)
+    end
+
+    def email_image_tag(image, **options)
+        attachments[image] = File.read(Rails.root.join("app/assets/images/DebateLogo.png"))
+        image_tag attachments[image].url, **options
+    end
+end
