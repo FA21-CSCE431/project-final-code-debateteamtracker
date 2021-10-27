644bcc14196c536401b5c3d06a3e7ed6b8042e17
root <root@localhost>
2021-10-27 01:47:36 +0000

    Fixed no login scenario not redirecting in profile_controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,7 +2,9 @@ class ProfileController < ApplicationController
     before_action :authenticate_permission
 
     def authenticate_permission
-      if Member.exists?(['email LIKE ? AND priority = 3', "%#{current_admin.email}"])
+      if !current_admin.present?
+        redirect_to '/', alert: 'If you are a member, sign in to view your profile'
+      elsif Member.exists?(['email LIKE ? AND priority = 3', "%#{current_admin.email}"])
         redirect_to '/admin_dashboard/overview'
       end
     end
