584a1405e7f7f40d82043a4637d084c7c4fdd819
root <root@localhost>
2021-11-09 18:35:34 +0000

    [MH Update] DEB 33 - Event previews

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
   validates :name, presence: true
   validates :location, presence: true
   validates :description, presence: true
-  validates :date, presence: true
-  validates :time, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       get :delete
     end
   end
+
   resources :points_events
   get 'pages/leaderboard'
   get 'profile/index'
@@ -20,6 +21,8 @@ Rails.application.routes.draw do
   resources :announcements
   get 'announcements/announcements'
   root to: 'pages#index'
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
