622b2c3b5033735871385da67f4b1dde88d776af
davidfasina <fasinadavid@gmail.com>
2021-11-09 15:40:13 -0600

    Hid personal points table for users not signed in. Inlcuded personal points table in profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -15,7 +15,23 @@ class ProfileController < ApplicationController
         else
             redirect_to '/', alert: 'If you are a member, sign in to view your profile'
         end
+        @entries = []
+        unless current_admin.nil?
+          if Member.exists?(['email LIKE ? AND priority = 1', "%#{current_admin.email}"])
+            @current = Member.where(:email => current_admin.email).first
+    
+            @participation = Participation.where(member_id: @current.id)
+            @events = PointsEvent.all
+        
+        
+        
+            @participation.each do |p|
+              @entries << @events.find(p.points_event_id)
+            end
+          end
+        end
     end
+
     def user
         @member = Member.where(:email => current_admin.email).limit(1)
     end
