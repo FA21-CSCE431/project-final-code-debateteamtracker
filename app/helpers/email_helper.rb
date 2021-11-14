d125f9cec3f42f395ff910fa2956c544161deff7
Harini Maheswaran <harinimaheswaran@client-10-228-213-36.tamulink.tamu.edu>
2021-11-14 17:18:48 -0600

    Update groups tests & anouncement message

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,3 @@
 module EmailHelper
-    def email_image_tag(image, **options)
-      attachments[image] = File.read(Rails.root.join("app/assets/images/DebateLogo.png"))
-      image_tag attachments[image].url, **options
-    end
-  end 
+
+end 
--- spec/feature/integration_groups_spec.rb
+++ spec/feature/integration_groups_spec.rb
@@ -2,11 +2,34 @@
 require 'rails_helper'
 
 RSpec.describe 'Writing a group', type: :feature do
+  before(:each) do
+    @admin = Admin.create(email: 'admin@gmail.com')
+    sign_in @admin
+  end
+  def make_admin
+      if Member.where(email: 'admin@gmail.com', priority: 3, points: 0, name: 'admin').first.nil?
+        Member.create!(email: 'admin@gmail.com', priority: 3, points: 0, name: 'admin')
+      end
+  end
+
   scenario 'valid inputs' do
+    make_admin
     visit new_group_path
     fill_in 'Title', with: 'Group 1'
-    click_on 'New Group'
+    click_on 'Create Group'
     visit groups_path
     expect(page).to have_content('Group 1')
+    click_on 'Edit'
+    fill_in 'Title', with: 'Group 2'
+    click_on 'Update Group'
+    expect(page).to have_content('Group 2')
+    click_on 'Back'
+    expect(page).to have_content('Group 2')
+    click_on 'Destroy'
+    expect(page).to have_no_content('Group 2')
+    
+
+
+    
   end
 end
